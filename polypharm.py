a3af0bde59a2c2290798b742f2c22592e222bf5b
Francisco Adasme <francisco.adasme@gmail.com>
2023-01-11 18:20:53 -0300

    Refactor ranking_compounds code

--- polypharm.py
+++ polypharm.py
@@ -85,6 +85,27 @@ def rank_poses(
     return df
 
 
+def ranking_compounds(results: pd.DataFrame) -> pd.DataFrame:
+    common_names: List[str] = set.intersection(
+        *[set(names) for names in results.groupby("PROTEIN")["NAME"].unique()]
+    )
+    results = results[results["NAME"].isin(common_names)]
+
+    rows = []
+    for [name, index], df in results.groupby(["NAME", "INDEX"]):
+        row_data = dict(NAME=name, INDEX=index)
+        for row in df.iterrows():
+            prot = row["PROTEIN"]
+            row_data[f"{prot}_INT"] = row["INT"]
+            row_data[f"{prot}_DGBIND"] = row["DGBIND"]
+            row_data[f"{prot}_NORMT"] = row["NORMT"]
+            row_data[f"{prot}_RANK"] = row["RANK"]
+        row_data["SUM_RANK"] = df["RANK"].sum()
+        row_data["SUM_NORMT"] = df["NORMT"].sum()
+        rows.append(row_data)
+    return pd.DataFrame(rows).sort_values("SUM_NORMT", ascending=True)
+
+
 def report(
     csvfile: PathLike, output_dir: PathLike, resids: str, contact_cutoff: float
 ) -> pd.DataFrame:
@@ -97,125 +118,6 @@ def report(
         residues=resids,
     )
     return pd.read_csv(csvfile)
-
-
-def ranking_compounds_old(
-    common_compounds, df_task1_rank, df_nav_rank, df_kv_cc_rank, df_kv_sp_rank, order
-):
-    new_df = pd.DataFrame()
-    for c in common_compounds:
-        task1_row = df_task1_rank.loc[df_task1_rank["NAME"].str.contains(f"{c}-out")]
-        nav_row = df_nav_rank.loc[df_nav_rank["NAME"].str.contains(f"{c}-out")]
-        kv_cc_row = df_kv_cc_rank.loc[df_kv_cc_rank["NAME"].str.contains(f"{c}-out")]
-        kv_sp_row = df_kv_sp_rank.loc[df_kv_sp_rank["NAME"].str.contains(f"{c}-out")]
-        new_rank = (
-            task1_row.RANK.iloc[0]
-            + nav_row.RANK.iloc[0]
-            + kv_cc_row.RANK.iloc[0]
-            + kv_sp_row.RANK.iloc[0]
-        )
-        new_norm_total = (
-            task1_row.NORMT.iloc[0]
-            + nav_row.NORMT.iloc[0]
-            + kv_cc_row.NORMT.iloc[0]
-            + kv_sp_row.NORMT.iloc[0]
-        )
-        task1_normt = task1_row.NORMT.iloc[0]
-        nav_normt = nav_row.NORMT.iloc[0]
-        kv_cc_normt = kv_cc_row.NORMT.iloc[0]
-        kv_sp_normt = kv_sp_row.NORMT.iloc[0]
-        task1_dgbind = task1_row.DGBIND.iloc[0]
-        nav_dgbind = nav_row.DGBIND.iloc[0]
-        kv_cc_dgbind = kv_cc_row.DGBIND.iloc[0]
-        kv_sp_dgbind = kv_sp_row.DGBIND.iloc[0]
-        task1_int = task1_row.INT.iloc[0]
-        nav_int = nav_row.INT.iloc[0]
-        kv_cc_int = kv_cc_row.INT.iloc[0]
-        kv_sp_int = kv_sp_row.INT.iloc[0]
-        new_row = {
-            "NAME": c,
-            "TASK1_INT": task1_int,
-            "NAV_INT": nav_int,
-            "KV_CC_INT": kv_cc_int,
-            "KV_SP_INT": kv_sp_int,
-            "TASK1_DGBIND": task1_dgbind,
-            "NAV_DGBIND": nav_dgbind,
-            "KV_CC_DGBIND": kv_cc_dgbind,
-            "KV_SP_DGBIND": kv_sp_dgbind,
-            "TASK1_NORMT": task1_normt,
-            "NAV_NORMT": nav_normt,
-            "KV_CC_NORMT": kv_cc_normt,
-            "KV_SP_NORMT": kv_sp_normt,
-            "SUM_NORMT": new_norm_total,
-            "SUM_RANK": new_rank,
-        }
-        new_row = pd.DataFrame(new_row, index=[0])
-        new_df = pd.concat([new_df, new_row], ignore_index=True)
-        if order == "SUM_NORM_TOTAL":
-            new_df = new_df.sort_values(by=["SUM_NORM_TOTAL"], ascending=False)
-        if order == "SUM_RANK":
-            new_df = new_df.sort_values(by=["SUM_RANK"], ascending=True)
-    new_df.reset_index(drop=True, inplace=True)
-    return new_df
-
-
-def ranking_compounds(
-    common_compounds: list,
-    dict_rank_set: dict,
-    order: str = "SUM_RANK",
-) -> pd.DataFrame:
-    new_row = {}
-    new_df = pd.DataFrame()
-    for c in common_compounds:
-        rows_list = list(
-            v.loc[v["NAME"].str.contains(f"{c}-out")]
-            for k, v in dict_rank_set.items()
-            if k.endswith("rank")
-        )
-        # print(rows_list)
-        prot_list = list(
-            k.replace("_rank", "")
-            for k, v in dict_rank_set.items()
-            if k.endswith("rank")
-        )
-        new_rank = sum(list(v.RANK.iloc[0] for v in rows_list))
-        new_norm_total = sum(list(v.NORMT.iloc[0] for v in rows_list))
-        normt_list = list(v.NORMT.iloc[0] for v in rows_list)
-        dgbind_list = list(v.DGBIND.iloc[0] for v in rows_list)
-        int_list = list(v.INT.iloc[0] for v in rows_list)
-        new_row[f"NAME"] = c
-        for i in range(len(prot_list)):
-            new_row[f"{prot_list[i]}_INT"] = int_list[i]
-            new_row[f"{prot_list[i]}_DGBIND"] = dgbind_list[i]
-            new_row[f"{prot_list[i]}_NORMT"] = normt_list[i]
-        new_row[f"SUM_NORMT"] = new_norm_total
-        new_row[f"SUM_RANK"] = new_rank
-
-        new_row = pd.DataFrame(new_row, index=[0])
-        new_df = pd.concat([new_df, new_row], ignore_index=True)
-        if order == "SUM_NORMT":
-            new_df = new_df.sort_values(by=["SUM_NORMT"], ascending=False)
-        if order == "SUM_RANK":
-            new_df = new_df.sort_values(by=["SUM_RANK"], ascending=True)
-
-    new_df.reset_index(drop=True, inplace=True)
-    new_df.index = new_df.index + 1
-    new_df.index.name = "POSITION"
-    return new_df
-
-
-def common_compounds_old(set1: set, set2: set, set3: set, set4: set) -> list:
-    common = list(set.intersection(set1, set2, set3, set4))
-    return common
-
-
-def common_compounds(dict_rank_set: dict) -> list:
-    common = list(
-        set.intersection(
-            *(set(v) for k, v in dict_rank_set.items() if k.endswith("set"))
-        )
-    )
-    return common
 
 
 def run_silent(
@@ -367,14 +269,14 @@ def analysis(
         RankingCriterion.NORMALIZED_CONTACTS,
         RankingCriterion.TOTAL_SCORE,
     ],
-):
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     os.chdir(working_folder)
     Path(f"{output_folder_name}").mkdir(parents=True, exist_ok=True)
     mmgbsa_output_path = os.path.abspath(mmgbsa_output_path)
     proteins = glob.glob(f"{mmgbsa_output_path}/**")
 
     # Create dict of every system
-    csv_data = {}
+    results: List[pd.DataFrame] = []
     for protein in proteins:
         os.chdir(f"{working_folder}/{output_folder_name}")
         protein_name = os.path.basename(f"{protein}")
@@ -391,16 +293,10 @@ def analysis(
             resids=bs_residues[f"{protein_name}.mae"],
             contact_cutoff=radius,
         )
+        df["PROTEIN"] = protein_name
+        df = rank_poses(df, rank_criteria)
+        results.append(df)
 
-        data_rank = rank_poses(df, rank_criteria)
-        data_set: Set[str] = set(data_rank["NAME"].unique())
-        csv_data[f"{protein_name}_rank"] = data_rank
-        csv_data[f"{protein_name}_set"] = data_set
-        print(" ")
-
-    # Ranking compounds
-    common = common_compounds(csv_data)
-    print("Common compounds for the systems:", common)
-    print("There are", len(common), "common compounds")
-    ranking = ranking_compounds(common_compounds=common, dict_rank_set=csv_data)
-    return ranking
+    df = pd.concat(results)
+    cross_df = ranking_compounds(df)
+    return df, cross_df
