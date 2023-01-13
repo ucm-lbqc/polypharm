a75e56f595a2a4eced425f06eff36d97b0937914
Francisco Adasme <francisco.adasme@gmail.com>
2023-01-13 17:34:25 -0300

    Better handle errors in concurrent_subprocess

--- polypharm/concurrency.py
+++ polypharm/concurrency.py
@@ -1,11 +1,13 @@
 import asyncio
 import dataclasses
 import os
-import subprocess
+import textwrap
 import threading
-from typing import Any, Coroutine, Dict, List, Optional
+from typing import Any, Coroutine, Dict, List, Optional, TypeVar
 
 from .helpers import PathLike, transient_dir
+
+T = TypeVar("T")
 
 
 @dataclasses.dataclass
@@ -17,10 +19,10 @@ class Command:
 
 
 # hack to run on a Jupyter Notebook (use existing run loop)
-def async_run(coro: Coroutine[Any, Any, None]) -> None:
+def async_run(coro: Coroutine[Any, Any, T]) -> T:
     class RunThread(threading.Thread):
         def run(self):
-            asyncio.run(coro)
+            self.retvalue = asyncio.run(coro)
 
     try:
         loop = asyncio.get_running_loop()
@@ -31,19 +33,21 @@ def async_run(coro: Coroutine[Any, Any, None]) -> None:
         thread = RunThread()
         thread.start()
         thread.join()
+        return thread.retvalue
     else:
         return asyncio.run(coro)
 
 
 async def concurrent_subprocess(
     commands: List[Command], tasks: int = 1, quiet: bool = False
-) -> None:
+) -> bool:
     async def worker():
         while True:
             cmd = await queue.get()
+            i = n_cmd - queue.qsize()
+            istr = "[{{:>{}}}/{{}}]".format(len(str(n_cmd))).format(i, n_cmd)
             if not quiet:
-                i = nCommands - queue.qsize()
-                print(f"[{i}/{nCommands}] Starting {cmd.jobid}...")
+                print(f"{istr} Starting {cmd.jobid}...")
             with transient_dir(cmd.workdir or os.getcwd()):
                 proc = await asyncio.create_subprocess_exec(
                     cmd.args[0],
@@ -51,20 +55,26 @@ async def concurrent_subprocess(
                     stdout=asyncio.subprocess.PIPE,
                     stderr=asyncio.subprocess.STDOUT,
                 )
-                stdout, stderr = await proc.communicate()
-                if proc.returncode != 0:
-                    raise subprocess.CalledProcessError(
-                        proc.returncode or 0, cmd.args, stdout, stderr
-                    )
+                stdout, _ = await proc.communicate()
                 queue.task_done()
+                if proc.returncode == 0:
+                    continue
+
+                await failed_queue.put(cmd)
+                if not quiet:
+                    output = textwrap.indent(
+                        stdout.decode().strip(), " " * (len(istr) + 1)
+                    )
+                    print(f"{istr} Failed {cmd.jobid} with output:\n{output}")
 
     queue: asyncio.Queue[Command] = asyncio.Queue()
-    nCommands = len(commands)
+    failed_queue: asyncio.Queue[Command] = asyncio.Queue()
+    n_cmd = len(commands)
     for cmd in commands:
         queue.put_nowait(cmd)
 
     if not quiet:
-        print(f"Running {nCommands} command(s) in {tasks} parallel task(s)...")
+        print(f"Running {n_cmd} command(s) in {tasks} parallel task(s)...")
     workers: List[asyncio.Task[Any]] = [
         asyncio.create_task(worker()) for _ in range(tasks)
     ]
@@ -73,3 +83,8 @@ async def concurrent_subprocess(
     for task in workers:
         task.cancel()
     await asyncio.gather(*workers, return_exceptions=True)
+
+    success = failed_queue.qsize() == 0
+    if not quiet and not success:
+        print("WARNING: Some commands have failed. Please check the output.")
+    return success
